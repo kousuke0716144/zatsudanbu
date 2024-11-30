6366ff213c41b6a93130287f4a6467dcf5bf9999
kousuke0716144 <kousuke_sirius_2007@yahoo.co.jp>
2024-11-30 22:51:49 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const app_id = "0958416f-2a9a-4fd3-a331-0ffce321116b" //skywayのアプリケーションID //skyway application ID
+const secret_key = "IRzvm8nXdhw40MrYyzGvgtzIadZDpepp4UapYaNrj5k=" //skywayのシークレットキー // skyway secret key
+let proximity = true;  //近接VCのtrue:有効/false:無効(デフォルトはtrue) //Proximity VC true:enable/false:disable (default: true)
+let password = false; //接続時のパスワードの有無(デフォルトはfalse) //Password in connection (default: false)
+let distance = 6; // 声の届く最大距離(デフォルトは6) //Max distance of sound (default: 6)
+const port = 19132; // websocketをlistenするポート //Port to listen
+const web_port = 8080; // 近接vcのwebサイトと接続するポート // Port to connect website
+let lang = "ja"; //言語(ja:日本語, en:英語) //Language (ja:Japanese, en:English)
+module.exports = { distance, app_id, secret_key, proximity, port, web_port, lang, password };
